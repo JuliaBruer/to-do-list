6bf77f6593d34f6c2c1a5b2bcf843cadaada1b53
Julia Rohrer <julia-rohrer@web.de>
2023-10-01 16:21:37 +0200

    revised version of html, css, js

--- script.js
+++ script.js
@@ -3,16 +3,14 @@ let todos = [];
 
 load();
 
-function init(){
+/** This function executes the two functions when the page is loaded. */
+function init() {
     imageSlider();
     renderNotes();
 }
 
-/**
- * this function renders the image slider
- *
- */
-function imageSlider(){
+/** This function renders the image slider. */
+function imageSlider() {
     setTimeout(function(){
         document.getElementById('img1').style = 'transform: translateX(-100%)';
         document.getElementById('img2').style = 'transform: translateX(0%)';
@@ -34,33 +32,39 @@ function imageSlider(){
     }, 9000);
 }
 
-// to do list starts here
-function addToDo(){
+/** This function ensures that the input fields are mandatory. */
+function addToDo() {
     let deadline = document.getElementById('inputDeadline').value;
     let todo = document.getElementById('inputToDo').value;
-    if (deadline == '' || todo == '') {
-        alert('Bitte tragen Sie etwas ein.')
+    
+    if (deadline === '' || todo === '') {
+        alert('Bitte tragen Sie etwas ein.');
     } else {
-        deadlines.push(deadline);
-        todos.push(todo);
+        let currentDate = new Date();
+        let selectedDate = new Date(deadline);
+
+        if (selectedDate < currentDate) {
+            alert('Sie können kein Datum in der Vergangenheit auswählen.');
+        } else {
+            deadlines.push(deadline);
+            todos.push(todo);
+            renderNotes();
+            save();
+        }
     }
-    renderNotes();
-    save();
+
     document.getElementById('inputDeadline').value = '';
     document.getElementById('inputToDo').value = '';
 }
     
-/**
- * this function renders the to do list
- *
- */
-function renderNotes(){
+/** This function renders the to do list. */
+function renderNotes() {
     document.getElementById('table').innerHTML = '';
     document.getElementById('table').innerHTML += `
         <tr>
-        <td class="data"><b>Frist</b></td>
-        <td class="data"><b>To Do</b></td>
-        <td class="data"></td>
+            <td class="data"><b>Frist</b></td>
+            <td class="data"><b>To Do</b></td>
+            <td class="data"><b>löschen</b></td>
         </tr>
     `;
 
@@ -71,7 +75,8 @@ function renderNotes(){
     }
 }
 
-function generateTableHTML(deadline, todo, i){
+/** This function creates the HTML when adding a new To Do. */
+function generateTableHTML(deadline, todo, i) {
     let date = new Date(deadline);
     let day = date.getDate().toString().padStart(2, '0');
     let month = (date.getMonth() + 1).toString().padStart(2, '0');
@@ -79,21 +84,27 @@ function generateTableHTML(deadline, todo, i){
     let formattedDate = `${day}.${month}.${year}`;
 
     return `
-            <tr>
+        <tr>
             <td class="data">${formattedDate}</td>
             <td class="data to-do-content">${todo}</td>
-            <td class="data"><button class="btn" onclick="deleteContact(${i})">löschen</button></td>
-            </tr>
-        `;
+            <td class="data">
+                <div  onclick="deleteToDo(${i})" class="trash-container">
+                    <img src="img/trash.png">
+                </div>
+            </td>
+        </tr>
+    `;
 }
 
-function deleteContact(i) {
+/** This function deletes the To Do when you click the button to delete. */
+function deleteToDo(i) {
     deadlines.splice(i, 1);
     todos.splice(i, 1);
     renderNotes();
     save();
 }
 
+/** This function saves the added To Dos in the local storage. */
 function save() {
     let deadlineSave = JSON.stringify(deadlines);
     localStorage.setItem('deadlines', deadlineSave);
@@ -101,10 +112,12 @@ function save() {
     localStorage.setItem('todos', todoSave);
 }
 
+/** This function loads the added To Dos from the local storage. */
 function load() {
     let deadlineSave = localStorage.getItem('deadlines');
     let todoSave = localStorage.getItem('todos');
-    if(deadlineSave && todoSave) {
+    
+    if (deadlineSave && todoSave) {
         deadlines = JSON.parse(deadlineSave);
         todos = JSON.parse(todoSave);
 	}
